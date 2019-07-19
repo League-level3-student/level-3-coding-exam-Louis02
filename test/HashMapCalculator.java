8b72c5675ea689050400eef51be15c15832d983c
League Student <league@iMac-34.attlocal.net>
2019-07-19 16:23:16 -0700

    working

--- test/HashMapCalculator.java
+++ test/HashMapCalculator.java
@@ -4,10 +4,15 @@ public class HashMapCalculator {
 
 	public Object commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
 		// TODO Auto-generated method stub
-		int ans  = 0;
-		for(String s : hashmap1.keySet()) {
-			if(hashmap1.get(s).contains((CharSequence) hashmap2.keySet())) {
-				ans = 2;
+		int ans = 0;
+		for (String s : hashmap1.keySet()) {
+			for (String a : hashmap1.values()) {
+				if (hashmap1.keySet().equals(hashmap2.get(s))) {
+					if (hashmap1.values().equals(hashmap2.get(a)))
+						System.out.println("here");
+					ans = 2;
+
+				}
 			}
 		}
 		return ans;
